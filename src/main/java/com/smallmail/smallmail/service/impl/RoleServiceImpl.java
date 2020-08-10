b74b908b85aa121370ae31cef725cdfcb1e7daa4
Andrewmazyar <maziarandriy@gmail.com>
2020-08-10 14:44:59 +0300

    remove unneccecary field

--- src/main/java/com/smallmail/smallmail/service/impl/RoleServiceImpl.java
+++ src/main/java/com/smallmail/smallmail/service/impl/RoleServiceImpl.java
@@ -3,14 +3,11 @@ package com.smallmail.smallmail.service.impl;
 import java.util.List;
 import com.smallmail.smallmail.service.RoleService;
 import com.smallmail.smallmail.dao.RoleDao;
-import com.smallmail.smallmail.model.entity.Role;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.smallmail.smallmail.model.entity.Role;delete unnececcary
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleServiceImpl implements RoleService {
-    private static final Logger LOGGER = LogManager.getLogger(RoleServiceImpl.class);
 
     private final RoleDao roleDao;
 
